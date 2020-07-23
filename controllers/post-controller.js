3af33c99942aa1e53f80e7659400821747aff005
TS22082 <ts22082@gmail.com>
2020-07-23 14:02:26 -0700

    set up create and get post routes

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,3 +1,20 @@
 const Post = require("../models/post");
 
-module.exports = {};
+module.exports = {
+  makePost: (req, res) =>
+    Post.create({
+      text: req.body.text,
+      authorId: req.body.authorId,
+    })
+      .then((newPost) => res.send(newPost))
+      .catch((err) => res.send(err)),
+
+  getPost: (req, res) =>
+    !req.query.id
+      ? Post.find()
+          .then((allPosts) => res.send(allPosts))
+          .catch((err) => res.send(err))
+      : Post.findById({ _id: req.query.id })
+          .then((post) => res.send(post))
+          .catch((err) => res.send(err)),
+};
--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,4 +1,11 @@
 const router = require("express").Router();
-const {} = require("../controllers/post-controller");
+const { makePost, getPost } = require("../controllers/post-controller");
+
+// Creates a post. Requires email and password
+router.post("/post", makePost);
+
+//gets all posts if req.query is not passed
+//gets specific post if req.query is passed
+router.get("/post", getPost);
 
 module.exports = router;
